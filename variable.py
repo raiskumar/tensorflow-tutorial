59f8da27c6f07957c0903b932a35615d1134a3e8
Kumar <vk83@INMAC111AH.local>
2018-01-28 23:55:31 +0530

    added alternative approach to variable initialization

--- variable.py
+++ variable.py
@@ -8,12 +8,13 @@ which have to be explicitly initialized and used in-graph to maintain state acro
 x = tf.constant(35, name='x')
 y = tf.Variable(x + 5, name='y')   # y = x + 5
 
-# Initialize all the variables;
+# Initialization of the variables must be run before all other operations in the model.i
 model = tf.global_variables_initializer()
+
+# model =tf.variables_initializer(var_list=[y])     # 2nd approach
 
 with tf.Session() as session:
     session.run(model)
     print(session.run(y))
     y = y + 1
     print(session.run(y))
-
